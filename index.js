9286c02b6bd221137384014b375ad236b0cacc7d
emwuuu <emwu212@gmail.com>
2018-08-08 16:12:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,4 @@ const welcomeMessage = "Welcome to Klossybook, " + currentUser + "!";
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-'Edsger Dijkstra'.slice();
-
-const shortGreeting = "Welcome, ${currentUser.slice(0,1)}!";
+const shortGreeting = "Welcome, currentUser.slice(0, 1)!";
